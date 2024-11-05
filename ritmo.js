b744fafea45b23744622f949546573ca18891702
Glitch (seis8s) <none>
2024-11-05 22:49:37 +0000

    🍱🪱 Updated with Glitch

--- ritmo.js
+++ ritmo.js
@@ -10,8 +10,14 @@ export function filtrarYaplanarParte(parteNonFlattened) {
 
 
 export function filtrarYaplanarParteCongas(parteNonFlattened) {
-      let parteFlattened = parteNonFlattened.flat();
-      let filteredParte = parteFlattened.filter(e => e.note !== null);
-      // filteredParte.forEach(e => e.note = 'C4');
-      return filteredParte;
+  let parteFlattened = parteNonFlattened.flat();
+  let filteredParte = parteFlattened.filter(e => e.note !== null);
+  
+  filteredParte.forEach(e => {
+    if (e.note === 1) {
+      e.note = 'C4';
     }
+  });
+
+  return filteredParte;
+}
