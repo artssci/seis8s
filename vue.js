53162564506291661714558a4dd05fac77259ca6
Glitch (seis8s) <none>
2024-11-05 20:12:57 +0000

    ⛽🌏 Updated with Glitch

--- vue.js
+++ vue.js
@@ -40,7 +40,6 @@ const app = Vue.createApp({
 
       // editor      
       panelIzquierdoEstaVisible: true,
-      ocultarPanelIzquierdo: false,
       showPlusSign: true,
       codeEditorImagenDeFondoXpos: 345.28,
       codeEditorXpos: 343.5,
@@ -650,14 +649,21 @@ replaceCommand(command) {
     console.log("normal")
     this.panelIzquierdoEstaVisible = true
       this.codeEditorImagenDeFondoXpos = 345.28
-      this.codeEditorXpo = 343.5
+      this.codeEditorXpos = 343.5
       this.consoleXpos = 343.606
+      // this.initXdocTab = 63.332373
+      this.initXCerrarDocIcono = 549.42                  
+      this.initXPlusSsign = 575.524
+
     } else {
       console.log("completa")
       this.panelIzquierdoEstaVisible = false
       this.codeEditorImagenDeFondoXpos = -0.432373
       this.codeEditorXpos =  -0.432373
       this.consoleXpos = -0.432373
+      this.initXdocTab = 63.332373
+      this.initXCerrarDocIcono= 205.816
+      this.initXPlusSsign = 231.918
       // x="-0.432373" y="199.5" 
         // width="342.932" height="469"
 
@@ -666,6 +672,7 @@ replaceCommand(command) {
 //     
   }
 });
+
 
 app.mount("#app");
 
